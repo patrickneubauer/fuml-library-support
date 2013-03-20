6208d21b73729c21ad2080f018a6426fba6e88cb
patrickneubauer <patymedia@gmail.com>
2013-03-20 21:51:52 +0100

    Incorporated DynamicClassLoader from basic-building-blocks

---
+++ external-library-support-fuml-test/src/org/modelexecution/fuml/extlib/test/DynamicClassLoaderTest.java
@@ -0,0 +1,293 @@
+package org.modelexecution.fuml.extlib.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+import org.modelexecution.fuml.extlib.DynamicClassLoader;
+
+/**
+ * Class that tests:
+ * <ul>
+ * <li>Create instances of Classes located in external Jar files</li>
+ * <li>Invoke methods of Classes located in external Jar files</li>
+ * </ul>
+ * 
+ * @author patrickneubauer
+ * 
+ */
+public class DynamicClassLoaderTest {
+
+	/**
+	 * Test that creates an instance and calls a method of a Class (Car) from
+	 * inside a JAR file using Java Reflection. The method's return type is a
+	 * String.
+	 */
+	@Test
+	public void carClassMethodTest() {
+		String jarPath = "extlibs/Vehicles.jar";
+		String classToLoad = "vehiclelibrary.Car";
+		String methodToLoad = "toString";
+
+		String[] jarPaths = new String[] { jarPath };
+		DynamicClassLoader dynamicClassLoader = new DynamicClassLoader(jarPaths);
+		ClassLoader classLoader = dynamicClassLoader.getClassLoader();
+
+		try {
+			Class<?> carClass = classLoader.loadClass(classToLoad);
+			Method toStringMethod = carClass.getDeclaredMethod(methodToLoad, null);
+			toStringMethod.setAccessible(true); // shut down security
+
+			Object carObject = carClass.newInstance();
+
+			String toStringReturnValue = (String) toStringMethod.invoke(carObject, null);
+			assertEquals("This car has 4 seats, 4 doors and 4 wheels.", toStringReturnValue);
+
+		} catch (ClassNotFoundException | NoSuchMethodException e) {
+			System.out.println("Couldn't find the requested Class and/or Method. Details: " + e);
+		} catch (InstantiationException e) {
+			System.out.println("Couldn't create an instance of the Class " + classToLoad + ". Details: " + e);
+		} catch (IllegalAccessException e) {
+			System.out.println("Couldn't create an instance of the Class " + classToLoad + " and/or invoke the Method " + methodToLoad
+					+ ". Details: " + e);
+		} catch (IllegalArgumentException | InvocationTargetException e) {
+			System.out.println("Couldn't invoke the Method " + methodToLoad + ". Details: " + e);
+		}
+	}
+
+	/**
+	 * Test that creates an instance and calls a method of a Class (Vehicle)
+	 * from inside a JAR file using Java Reflection. The method's return type is
+	 * an Enum.
+	 */
+	@Test
+	public void vehicleClassEnumReturnNoTurnTest() {
+		String jarPath = "extlibs/Vehicles.jar";
+		String classToLoad = "vehiclelibrary.Vehicle";
+		String methodToLoad = "lastTurn";
+
+		String[] jarPaths = new String[] { jarPath };
+		DynamicClassLoader dynamicClassLoader = new DynamicClassLoader(jarPaths);
+		ClassLoader classLoader = dynamicClassLoader.getClassLoader();
+
+		try {
+			Class<?> vehicleClass = classLoader.loadClass(classToLoad);
+			Method lastTurnMethod = vehicleClass.getDeclaredMethod(methodToLoad, null);
+			lastTurnMethod.setAccessible(true); // shut down security
+
+			Object vehicleObject = vehicleClass.newInstance();
+
+			Enum lastTurnReturnValue = (Enum) lastTurnMethod.invoke(vehicleObject, null);
+			assertEquals("NOTURN", lastTurnReturnValue.toString());
+
+		} catch (ClassNotFoundException | NoSuchMethodException e) {
+			System.out.println("Couldn't find the requested Class and/or Method. Details: " + e);
+		} catch (InstantiationException e) {
+			System.out.println("Couldn't create an instance of the Class " + classToLoad + ". Details: " + e);
+		} catch (IllegalAccessException e) {
+			System.out.println("Couldn't create an instance of the Class " + classToLoad + " and/or invoke the Method " + methodToLoad
+					+ ". Details: " + e);
+		} catch (IllegalArgumentException | InvocationTargetException e) {
+			System.out.println("Couldn't invoke the Method " + methodToLoad + ". Details: " + e);
+		}
+	}
+
+	/**
+	 * Test that creates an instance and calls 2 methods of a Class (Vehicle)
+	 * from inside a JAR file using Java Reflection. The 1st method modifies a
+	 * member variable of the object and returns it. The 2nd method (only)
+	 * returns the modified member variable of the object. At the end both are
+	 * compared, s.t. the test only succeeds if both return values are equal.
+	 */
+	@Test
+	public void vehicleClassEnumReturnSomeTurnTest() {
+		String jarPath = "extlibs/Vehicles.jar";
+		String classToLoad = "vehiclelibrary.Vehicle";
+
+		String[] jarPaths = new String[] { jarPath };
+		DynamicClassLoader dynamicClassLoader = new DynamicClassLoader(jarPaths);
+		ClassLoader classLoader = dynamicClassLoader.getClassLoader();
+
+		try {
+			Class<?> vehicleClass = classLoader.loadClass(classToLoad);
+			Method nextTurnMethod = vehicleClass.getDeclaredMethod("nextTurn", null);
+			nextTurnMethod.setAccessible(true); // shut down security
+
+			Object vehicleObject = vehicleClass.newInstance();
+
+			Enum nextTurnReturnValue = (Enum) nextTurnMethod.invoke(vehicleObject, null);
+			assertFalse(nextTurnReturnValue.toString().equals("NOTURN"));
+
+			// check if the last turn equals the next turn just established
+			// above
+			Method lastTurnMethod = vehicleClass.getDeclaredMethod("lastTurn", null);
+			Enum lastTurnReturnValue = (Enum) lastTurnMethod.invoke(vehicleObject, null);
+			assertSame(nextTurnReturnValue, lastTurnReturnValue);
+
+		} catch (ClassNotFoundException | NoSuchMethodException e) {
+			System.out.println("Couldn't find the requested Class and/or Method. Details: " + e);
+		} catch (InstantiationException e) {
+			System.out.println("Couldn't create an instance of the Class " + classToLoad + ". Details: " + e);
+		} catch (IllegalAccessException e) {
+			System.out.println("Couldn't create an instance of the Class " + classToLoad + " and/or invoke the Method. Details: " + e);
+		} catch (IllegalArgumentException | InvocationTargetException e) {
+			System.out.println("Couldn't invoke a Method. Details: " + e);
+		}
+	}
+
+	/**
+	 * Test that creates an instance and calls a method of a Class (Truck) from
+	 * inside a JAR file using Java Reflection. The instance of Truck is created
+	 * by calling its non-default constructor with a primitive parameter
+	 * (boolean).
+	 * 
+	 * Instead of <code>
+	 *   Method someMethod = someClass.getDeclaredMethod("methodName", parametersToPass);  
+	 * </code>
+	 * 
+	 * we do (example) <code>
+	 *   Class[] constructorParameters = new Class[]{Boolean.TYPE};
+	 *   Constructor someConstructor = someClass.getDeclaredConstructor(constructorParameters);  
+	 * </code>
+	 */
+	@Test
+	public void truckClassPassParametersToConstructorTest() {
+		String jarPath = "extlibs/Vehicles.jar";
+		String classToLoad = "vehiclelibrary.Truck";
+
+		String[] jarPaths = new String[] { jarPath };
+		DynamicClassLoader dynamicClassLoader = new DynamicClassLoader(jarPaths);
+		ClassLoader classLoader = dynamicClassLoader.getClassLoader();
+
+		try {
+			Class<?> truckClass = classLoader.loadClass(classToLoad);
+			Class[] constructorParameters = new Class[] { Boolean.TYPE };
+
+			// Provide the parameter(s) with value(s)
+			boolean isPickup = false; // by default it is a pickup truck
+			Object[] params = new Object[] { new Boolean(isPickup) };
+
+			Constructor truckConstructorWithPickupSpecification = truckClass.getDeclaredConstructor(constructorParameters);
+			truckConstructorWithPickupSpecification.setAccessible(true); // shut
+																			// down
+																			// security
+
+			// create an instance of Truck by passing a parameter to its
+			// constructor
+			Object truckObject = truckConstructorWithPickupSpecification.newInstance(isPickup);
+
+			// check if truckObject is not a pickup truck
+			Method toStringMethod = truckClass.getDeclaredMethod("toString", null);
+			String toStringReturnValue = (String) toStringMethod.invoke(truckObject, null);
+			// Note: a pickup truck is called "pickup-truck" in the toString
+			// method
+			// while a non pickup truck is simply called "truck"
+			assertEquals("This truck has 4 seats, 4 doors and 4 wheels.", toStringReturnValue);
+
+		} catch (ClassNotFoundException | NoSuchMethodException e) {
+			System.out.println("Couldn't find the requested Class and/or Method. Details: " + e);
+		} catch (InstantiationException e) {
+			System.out.println("Couldn't create an instance of the Class " + classToLoad + ". Details: " + e);
+		} catch (IllegalAccessException e) {
+			System.out.println("Couldn't create an instance of the Class " + classToLoad + " and/or invoke the Method. Details: " + e);
+		} catch (IllegalArgumentException | InvocationTargetException e) {
+			System.out.println("Couldn't invoke a Method. Details: " + e);
+		}
+
+	}
+
+	/**
+	 * Test that accesses a method in a class that is inherited from a super
+	 * class
+	 * 
+	 * Requires a "getSuperClass()" call: <code>
+	 *   whatEverClass.getSuperclass().getDeclaredMethod("methodName", parametersToPass);
+	 * </code>
+	 */
+	@Test
+	public void carClassInheritedMethodTest() {
+		String jarPath = "extlibs/Vehicles.jar";
+		String methodToLoad = "lastTurn";
+
+		String[] jarPaths = new String[] { jarPath };
+		DynamicClassLoader dynamicClassLoader = new DynamicClassLoader(jarPaths);
+		ClassLoader classLoader = dynamicClassLoader.getClassLoader();
+
+		try {
+			Class<?> carClass = classLoader.loadClass("vehiclelibrary.Car");
+			Method lastTurnMethod = carClass.getSuperclass().getDeclaredMethod(methodToLoad, null);
+			lastTurnMethod.setAccessible(true); // shut down security
+
+			Object carObject = carClass.newInstance();
+
+			Enum lastTurnReturnValue = (Enum) lastTurnMethod.invoke(carObject, null);
+			assertEquals("NOTURN", lastTurnReturnValue.toString());
+
+		} catch (ClassNotFoundException | NoSuchMethodException e) {
+			System.out.println("Couldn't find the requested Class and/or Method. Details: " + e);
+		} catch (InstantiationException e) {
+			System.out.println("Couldn't create an instance of the Class. Details: " + e);
+		} catch (IllegalAccessException e) {
+			System.out.println("Couldn't create an instance of the Class and/or invoke the Method " + methodToLoad + ". Details: " + e);
+		} catch (IllegalArgumentException | InvocationTargetException e) {
+			System.out.println("Couldn't invoke the Method " + methodToLoad + ". Details: " + e);
+		}
+	}
+
+	/**
+	 * Test that accesses a private field in a class that is inherited from a
+	 * super class
+	 * 
+	 * Example if the field is of type "String": <code>
+	 *   Field someField = someClass.getSuperclass().getDeclaredField("fieldName");
+	 *   ...
+	 *   String someFieldReturnValue = (String) someField.get(someObjectOfClass);
+	 * </code>
+	 */
+	@Test
+	public void carClassInheritedPrivateFieldTest() {
+		String jarPath = "extlibs/Vehicles.jar";
+		String fieldToLoad = "vehicleId";
+
+		String[] jarPaths = new String[] { jarPath };
+		DynamicClassLoader dynamicClassLoader = new DynamicClassLoader(jarPaths);
+		ClassLoader classLoader = dynamicClassLoader.getClassLoader();
+
+		try {
+			Class<?> carClass = classLoader.loadClass("vehiclelibrary.Car");
+			Field vehicleIdField = carClass.getSuperclass().getDeclaredField(fieldToLoad);
+			vehicleIdField.setAccessible(true); // shut down security
+
+			Object carObject = carClass.newInstance();
+
+			// reading from a private Field
+			String vehicleIdReturnValue = (String) vehicleIdField.get(carObject);
+			assertTrue(vehicleIdReturnValue.contains("vehicle_"));
+
+			// writing to a private Field
+			vehicleIdField.set(carObject, "vehicle_007");
+			// reading the value back from the private Field to check if the
+			// writing was successful
+			String newVehicleIdReturnValue = (String) vehicleIdField.get(carObject);
+			assertEquals("vehicle_007", newVehicleIdReturnValue);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("Couldn't find the requested Class and/or Method. Details: " + e);
+		} catch (InstantiationException e) {
+			System.out.println("Couldn't create an instance of the Class. Details: " + e);
+		} catch (IllegalAccessException e) {
+			System.out.println("Couldn't create an instance of the Class and/or get the Field " + fieldToLoad + ". Details: " + e);
+		} catch (IllegalArgumentException | NoSuchFieldException e) {
+			System.out.println("Couldn't get the Field " + fieldToLoad + ". Details: " + e);
+		}
+
+	}
+
+}
--- external-library-support-fuml/src/org/modelexecution/fuml/extlib/DynamicClassLoader.java
+++ external-library-support-fuml/src/org/modelexecution/fuml/extlib/DynamicClassLoader.java
@@ -8,41 +8,48 @@ import java.net.URL;
 import java.net.URLClassLoader;
 
 /**
- * Class that creates an instance of {@link ClassLoader} as its member
- * by taking file path(s) to JAR file(s)
- *
+ * Class that creates an instance of {@link ClassLoader} as its member by taking
+ * file path(s) to JAR file(s)
+ * 
  * @author Patrick Neubauer
- *
+ * 
  */
 public class DynamicClassLoader {
-	
-	private URL[] jarURLs; // ABSOLUTE paths linking to JAR files in the file system
+
+	private URL[] jarURLs; // ABSOLUTE paths linking to JAR files in the file
+							// system
 	private ClassLoader classLoader;
-	
+
 	/**
-	 * Creates an instance of the class from which a {@link ClassLoader} can be obtained
+	 * Creates an instance of the class from which a {@link ClassLoader} can be
+	 * obtained
 	 * 
-	 * @param jarPaths RELATIVE paths pointing to the JAR file in the file system
+	 * @param jarPaths
+	 *            RELATIVE paths pointing to the JAR file in the file system
 	 */
 	public DynamicClassLoader(String... jarPaths) {
 		jarURLs = getJarPaths(jarPaths);
 		classLoader = new URLClassLoader(jarURLs);
 	}
-	
+
 	/**
-	 * Get an array of URLs pointing to the JAR file(s) in the file system using ABSOLUTE path(s)
+	 * Get an array of URLs pointing to the JAR file(s) in the file system using
+	 * ABSOLUTE path(s)
 	 * 
-	 * @param jarPaths String array of RELATIVE paths linking to the JAR location in the file system
+	 * @param jarPaths
+	 *            String array of RELATIVE paths linking to the JAR location in
+	 *            the file system
 	 * @return ABSOLUTE paths linking to JAR files in the file system
 	 */
 	private URL[] getJarPaths(String[] jarPaths) {
 		URL[] urls = new URL[jarPaths.length];
-		
-		for(int i=0; i < jarPaths.length; i++) {
+
+		for (int i = 0; i < jarPaths.length; i++) {
 			try {
 				urls[i] = new File(jarPaths[i]).toURI().toURL();
 			} catch (MalformedURLException e) {
-				System.out.println("A JAR path (" + jarPaths[i] + ") couldn't be added to the array of URLs pointing to the file in the file system. Details: " + e);
+				System.out.println("A JAR path (" + jarPaths[i]
+						+ ") couldn't be added to the array of URLs pointing to the file in the file system. Details: " + e);
 			}
 		}
 
@@ -52,7 +59,8 @@ public class DynamicClassLoader {
 	/**
 	 * Get a {@link ClassLoader} to load a Class from
 	 * 
-	 * @return the classLoader from which Classes of the loaded JAR file(s) can be loaded
+	 * @return the classLoader from which Classes of the loaded JAR file(s) can
+	 *         be loaded
 	 */
 	public ClassLoader getClassLoader() {
 		return classLoader;
