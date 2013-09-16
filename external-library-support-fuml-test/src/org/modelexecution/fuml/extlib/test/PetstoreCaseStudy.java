b5d21b707fbb2055188a22c9a582421af5a9364b
patrickneubauer <patymedia@gmail.com>
2013-09-16 17:04:11 +0200

    Adds tests for scenario7Customer and scenario7 to PetstoreCaseStudy
    Tests cover the use of the IntegrationLayer's ExecutionContext directly
    and the use of the IntegrationLayer's ExecutionContext via the
    PapyrusModelILExecutor

--- external-library-support-fuml-test/src/org/modelexecution/fuml/extlib/test/PetstoreCaseStudy.java
+++ external-library-support-fuml-test/src/org/modelexecution/fuml/extlib/test/PetstoreCaseStudy.java
@@ -3,6 +3,9 @@
  */
 package org.modelexecution.fuml.extlib.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -14,31 +17,26 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.papyrus.infra.core.sashwindows.di.PageList;
-import org.eclipse.papyrus.infra.core.sashwindows.di.SashWindowsMngr;
 import org.eclipse.papyrus.infra.core.sashwindows.di.util.DiResourceFactoryImpl;
 import org.eclipse.uml2.uml.Activity;
-import org.eclipse.uml2.uml.NamedElement;
-import org.eclipse.uml2.uml.UMLPackage;
-import org.eclipse.uml2.uml.resource.UMLResource;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.modelexecution.fuml.convert.ConverterRegistry;
 import org.modelexecution.fuml.convert.IConversionResult;
-import org.modelexecution.fuml.convert.IConverter;
 import org.modelexecution.fuml.convert.uml2.UML2Converter;
 import org.modelexecution.fuml.extlib.IntegrationLayer;
 import org.modelexecution.fuml.extlib.IntegrationLayerImpl;
 import org.modelexecution.fuml.extlib.UML2Preparer;
 import org.modelexecution.fuml.extlib.papyrus.PapyrusModelILExecutor;
 import org.modelexecution.fumldebug.core.ExecutionEventListener;
+import org.modelexecution.fumldebug.core.event.ActivityEntryEvent;
 import org.modelexecution.fumldebug.core.event.Event;
 import org.modelexecution.fumldebug.core.event.ExtensionalValueEvent;
 import org.modelexecution.fumldebug.core.trace.tracemodel.Trace;
-import org.modelexecution.fumldebug.papyrus.util.DiResourceUtil;
 
+import fUML.Semantics.Classes.Kernel.Reference;
+import fUML.Semantics.CommonBehaviors.BasicBehaviors.ParameterValue;
 import fUML.Semantics.CommonBehaviors.BasicBehaviors.ParameterValueList;
 import fUML.Syntax.Actions.BasicActions.CallBehaviorAction;
 import fUML.Syntax.Activities.CompleteStructuredActivities.StructuredActivityNode;
@@ -49,6 +47,8 @@ import fUML.Syntax.CommonBehaviors.BasicBehaviors.OpaqueBehavior;
 
 /**
  * Integration Test (IT) Class for {@link IntegrationLayer} on the Petstore Case Study
+ * It contains tests that use the IntegrationLayer's ExecutionContext directly and
+ * tests that use the IntegrationLayer's ExecutionContext via the PapyrusModelILExecutor
  * 
  * @author Patrick Neubauer
  * 
@@ -192,8 +192,7 @@ public class PetstoreCaseStudy implements ExecutionEventListener {
 	// -------------------
 
 	/**
-	 * Tests {@link CreateObjectAction} that invokes an Object from an external
-	 * library and a {@link CallOperationAction} on the invoked Object setting an input value field AND returning an output value
+	 * Tests the Petstore Scenario7 using the IntegrationLayer's ExecutionContext directly
 	 */
 	@Test
 	public void petstoreScenario7Test() {		
@@ -219,24 +218,117 @@ public class PetstoreCaseStudy implements ExecutionEventListener {
 		// Execute fUML Activity
 		integrationLayer.getExecutionContext().execute(fUMLActivity, null, new ParameterValueList());
 
-//		// Check if correct output ParameterValue exists in the
-//		// IntegrationLayer's ExecutionContext.activityExecutionOutput
-//		ParameterValue outputParameterValue = null;
-//
-//		for (Event event : eventlist) {
-//			if (event.toString().contains("ActivityNodeEntryEvent node = SendCallOperationAction")) {
-//				ActivityNodeEntryEvent activityNodeEntryEvent = (ActivityNodeEntryEvent) event;
-//				outputParameterValue = (ParameterValue) integrationLayer.getExecutionContext()
-//						.getActivityOutput(activityNodeEntryEvent.getActivityExecutionID()).get(0);
-//			}
-//		}
-//
-//		assertTrue(outputParameterValue != null);
-//		assertTrue(outputParameterValue.values.get(0) instanceof StringValue);
-//		// Check if operation's return value is correct (!)
-//		assertTrue(((StringValue) outputParameterValue.values.get(0)).value.contains("JavaMail")); // otherwise it would throw an exception
+		// Check if correct output ParameterValue exists in the
+		// IntegrationLayer's ExecutionContext.activityExecutionOutput
+		ParameterValue outputParameterValue = null;
+
+		for (Event event : eventlist) {
+			if (event.toString().contains("ActivityEntryEvent activity = scenario7")) {
+				ActivityEntryEvent activityEntryEvent = (ActivityEntryEvent) event;
+				outputParameterValue = (ParameterValue) integrationLayer.getExecutionContext()
+						.getActivityOutput(activityEntryEvent.getActivityExecutionID()).get(0);
+			}
+		}
+
+		assertTrue(outputParameterValue != null);
+		
+		assertTrue(outputParameterValue.values.get(0) instanceof Reference);
+		Reference orderReference = (Reference) outputParameterValue.values.get(0);
+		
+		assertTrue(orderReference.referent.getTypes().get(0).name.equals("Order"));
+		
+		assertEquals("orderDate", orderReference.getFeatureValues().get(0).feature.name);
+		assertEquals("orderLines", orderReference.getFeatureValues().get(1).feature.name);
+		assertEquals("customer", orderReference.getFeatureValues().get(2).feature.name);
 	}
 	
+	/**
+	 * Tests the Petstore Scenario7Customer using the IntegrationLayer's ExecutionContext directly
+	 */
+	@Test
+	public void petstoreScenario7CustomerTest() {		
+		String externalUmlFilePath = "models/petstoreCaseStudy/commons-email-1.3.1Converted.uml";
+		String activityDiagramFilePath = "models/petstoreCaseStudy/petstore.uml";
+		String activityName = "scenario7Customer";
+		
+		// Convert
+		Resource resource = getResource(activityDiagramFilePath, externalUmlFilePath);
+		UML2Converter uml2Converter = new UML2Converter();
+		IConversionResult conversionResult2 = uml2Converter.convert(resource.getContents().get(0));
+		
+		// Replace Opaque Behaviors
+		Iterator<fUML.Syntax.Activities.IntermediateActivities.Activity> iterator = conversionResult2.getActivities().iterator();
+		while(iterator.hasNext()) {
+			fUML.Syntax.Activities.IntermediateActivities.Activity activity = iterator.next();
+			replaceOpaqueBehaviors(activity);
+		}
+
+		// Get Activity
+		fUML.Syntax.Activities.IntermediateActivities.Activity fUMLActivity = conversionResult2.getActivity(activityName);
+		
+		// Execute fUML Activity
+		integrationLayer.getExecutionContext().execute(fUMLActivity, null, new ParameterValueList());
+
+		// Check if correct output ParameterValue exists in the
+		// IntegrationLayer's ExecutionContext.activityExecutionOutput
+		ParameterValue outputParameterValue = null;
+
+		for (Event event : eventlist) {
+			if (event.toString().contains("ActivityEntryEvent activity = scenario7Customer")) {
+				ActivityEntryEvent activityEntryEvent = (ActivityEntryEvent) event;
+				outputParameterValue = (ParameterValue) integrationLayer.getExecutionContext()
+						.getActivityOutput(activityEntryEvent.getActivityExecutionID()).get(0);
+			}
+		}
+
+		assertTrue(outputParameterValue != null);
+		
+		assertTrue(outputParameterValue.values.get(0) instanceof Reference);
+		Reference customerReference = (Reference) outputParameterValue.values.get(0);
+		
+		assertTrue(customerReference.referent.getTypes().get(0).name.equals("Customer"));
+		
+		assertEquals("login", customerReference.getFeatureValues().get(0).feature.name);
+		assertEquals("liz", customerReference.getFeatureValues().get(0).values.get(0).toString());
+		
+		assertEquals("password", customerReference.getFeatureValues().get(1).feature.name);
+		assertEquals("liz", customerReference.getFeatureValues().get(1).values.get(0).toString());
+		
+		
+	}
+	
+	/**
+	 * Tests the Petstore Scenario7 using the IntegrationLayer's ExecutionContext via the PapyrusModelILExecutor
+	 */
+	@Test
+	public void petstoreScenario7CustomerPapyrusTest() {
+		PapyrusModelILExecutor executor = new PapyrusModelILExecutor(
+				"models/petstoreCaseStudy/petstore.di");
+		clearLocus(executor);
+
+		Trace trace = executor.executeActivity("scenario7Customer", null, null);
+		ParameterValueList output = executor.getExecutionContext()
+				.getActivityOutput(
+						trace.getActivityExecutions().get(0)
+								.getActivityExecutionID());
+
+		assertTrue(output != null);
+		
+		assertTrue(output.get(0).values.get(0) instanceof Reference);
+		Reference customerReference = (Reference) output.get(0).values.get(0);
+		
+		assertTrue(customerReference.referent.getTypes().get(0).name.equals("Customer"));
+		
+		assertEquals("login", customerReference.getFeatureValues().get(0).feature.name);
+		assertEquals("liz", customerReference.getFeatureValues().get(0).values.get(0).toString());
+		
+		assertEquals("password", customerReference.getFeatureValues().get(1).feature.name);
+		assertEquals("liz", customerReference.getFeatureValues().get(1).values.get(0).toString());
+	}
+	
+	/**
+	 * Tests the Petstore Scenario7 using the IntegrationLayer's ExecutionContext via the PapyrusModelILExecutor
+	 */
 	@Test
 	public void petstoreScenario7PapyrusTest() {
 		PapyrusModelILExecutor executor = new PapyrusModelILExecutor(
@@ -248,6 +340,17 @@ public class PetstoreCaseStudy implements ExecutionEventListener {
 				.getActivityOutput(
 						trace.getActivityExecutions().get(0)
 								.getActivityExecutionID());
+		
+		assertTrue(output != null);
+		
+		assertTrue(output.get(0).values.get(0) instanceof Reference);
+		Reference orderReference = (Reference) output.get(0).values.get(0);
+		
+		assertTrue(orderReference.referent.getTypes().get(0).name.equals("Order"));
+		
+		assertEquals("orderDate", orderReference.getFeatureValues().get(0).feature.name);
+		assertEquals("orderLines", orderReference.getFeatureValues().get(1).feature.name);
+		assertEquals("customer", orderReference.getFeatureValues().get(2).feature.name);
 	}
 
 }
